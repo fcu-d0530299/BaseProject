524ff5b5697d179998a9a6636c89889fa768029d
John Doe <johndoe@example.com>
2017-12-06 12:08:02 +0800

    123

---
+++ src/test/java/com/github/d0530299/demoTest.java
@@ -0,0 +1,17 @@
+package com.github.d0530299;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+
+import static org.junit.Assert.*;
+
+public class demoTest {
+    @Test
+    public void getType() throws Exception {
+        demo triange = new demo(5,10,5);
+        String result = triange.getType();
+        Assert.assertEquals("不是三角形",result);
+    }
+
+}
