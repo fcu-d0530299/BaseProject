b5e5967a1718417ae6460e545aa0d1a1f7b678c3
John Doe <johndoe@example.com>
2017-12-06 12:35:33 +0800

    123

--- src/test/java/com/github/d0530299/demoTest.java
+++ src/test/java/com/github/d0530299/demoTest.java
@@ -11,6 +11,7 @@ public class demoTest {
     public void getType() throws Exception {
         demo triange = new demo(5,10,5);
         String result = triange.getType();
+
         Assert.assertEquals("不是三角形",result);
     }
 
